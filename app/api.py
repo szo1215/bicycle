b515a92927a58c46cdb59eaf1bc370209f858016
park <szo12151@gmail.com>
2015-09-29 18:47:49 +0900

    GPS Form 추가

--- app/api.py
+++ app/api.py
@@ -2,6 +2,7 @@
 from flask import jsonify, redirect, request, Blueprint
 
 from app import db
+from forms import GPSForm
 from models import GPS, User
 
 api = Blueprint('api', __name__, template_folder='templates')
@@ -9,32 +10,15 @@ api = Blueprint('api', __name__, template_folder='templates')
 
 @api.route('/gps/', methods=['POST'])
 def post_gps():
-    try:
-        j = json.loads(request.data)
-        request.values.get('')
-        # FIXME: 임시로 한개의 데이터만 받도록 수정 후에 다시 원복
-        """
-        for k, v in j.items():
-            u = GPS(latitude=v.get('latitude'),
-                    longtitude=v.get('longtitude'),
-                    altitude=v.get('altitude'),
-                    horizontal_accuracy=v.get('horizontalAccuracy'),
-                    vertical_accuracy=v.get('verticalAccuracy'),
-                    timestamp=v.get('timestamp'))
-            db.session.add(u)
-            db.session.commit()
-        """
-        gps = GPS(latitude=j.get('latitude'),
-                  longtitude=j.get('longtitude'),
-                  altitude=j.get('altitude'),
-                  horizontal_accuracy=j.get('horizontalAccuracy'),
-                  vertical_accuracy=j.get('verticalAccuracy'))
+    form = GPSForm()
+    if form.validate_on_submit():
+        gps = GPS()
+        form.populate_obj(gps)
         db.session.add(gps)
         db.session.commit()
         return jsonify(result="success")
-    except ValueError:
-        return jsonify(result="fail")
-
+    return jsonify(result="fail")
+    
 
 @api.route('/gps/', methods=['GET'])
 def get_gps():
--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask_wtf import Form
-from wtforms.fields import TextField, PasswordField
+from wtforms.fields import DecimalField, TextField, PasswordField
 from wtforms.validators import Email, Length
 
 
@@ -10,3 +10,13 @@ class LoginForm(Form):
     email = TextField(u'이메일', [Email(message='이메일 형식이 아닙니다.')])
     name = TextField(u'이름', [Length(max=10)])
     password = PasswordField(u'비밀번호', [Length(min=10, max=15)])
+
+
+# GPS Form
+class GPSForm(Form):
+    latitude = DecimalField(label=u'위도', places=8)
+    longtitude = DecimalField(label=u'경도', places=8)
+    altitude = DecimalField(label=u'고도', places=8)
+    horizontal_accuracy = DecimalField(label=u'수평 정확도', places=8)
+    vertical_accuracy = DecimalField(label=u'수직 정확도', places=8)
+
