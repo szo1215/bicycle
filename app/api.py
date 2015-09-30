3f27608cdc39fc92b063f95ec9c9ce55e7975022
park <szo12151@gmail.com>
2015-09-30 11:39:25 +0900

    로그인 api 추가

--- app/api.py
+++ app/api.py
@@ -1,11 +1,13 @@
 # -*- coding:utf-8 -*-
 from flask import jsonify, redirect, request, Blueprint
+from sqlalchemy import and_
 
 from app import db
 from forms import GPSForm
 from models import GPS, User
 
-api = Blueprint('api', __name__, template_folder='templates')
+api = Blueprint('api', __name__, template_folder='templates', 
+                url_prefix='/api')
 
 
 @api.route('/gps/', methods=['POST'])
@@ -27,3 +29,14 @@ def get_gps():
         {c.name: getattr(last, c.name) for c in last.__table__.columns}
     )
 
+
+@api.route('/sign_in', methods=['POST'])
+def post_sign_in():
+    email = request.values.get('email')
+    password = request.values.get('password')
+    login = db.session.query(User).filter_by(and_(email=email,
+                                                  password=password)).first()
+    if login is not None:
+        session['user'] = login.id
+        return jsonify(result='success', user_id=login.id)
+    return jsonify(result='fail')
