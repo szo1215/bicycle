c9b545274e4d500ed79e03c9eab24ce9416040a2
park <szo12151@gmail.com>
2015-11-10 14:13:42 +0900

    fabric deploy 추가

--- fabfile.py
+++ fabfile.py
@@ -1,14 +1,22 @@
-from fabric.api import env, run
+from fabric.contrib.files import exists
+from fabric.context_managers import cd
+from fabric.operations import run, sudo
+from fabric.state import env
 
-env.hosts = ['ec2-52-192-43-64.ap-northeast-1.compute.amazonaws.com']
 env.user = 'ubuntu'
 env.key_filename = ['Kya.pem']
+env.hosts = ['ec2-52-192-43-64.ap-northeast-1.compute.amazonaws.com']
+env.home = '/home/' + env.user
+env.project_name = 'bicycle'
+env.project_path = env.home + '/' + env.project_name
+env.static_path = env.project_path + '/static'
 
 
 def deploy():
-    run('cd bicycle')
-    run('. env/bin/activate')
-    run('git pull upstream master')
-    run('alembic upgrade head')
-    run('nohup python run.py &')
+    if exists(env.project_path):
+        with cd(env.project_path):
+            run('. env/bin/activate')
+            sudo('pip install -e .')
+            run('git pull upstream master')
+            run('alembic upgrade head')
 
